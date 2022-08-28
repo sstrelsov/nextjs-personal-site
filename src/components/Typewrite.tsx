db29688e7d096bd6c424938e3e6941ff02a64f51
Spencer Strelsov <spencer@casetext.com>
2022-08-28 13:57:17 -0400

    Reorganizes dir and updates gitignore

--- src/components/Typewrite.tsx
+++ src/components/Typewrite.tsx
@@ -31,43 +31,44 @@ const Typewriter = ({ phrases, variation }: TypewriterProps) => {
   const addNewLine = () => {
     setTimeout(() => {
       setNewLine(true);
-      setCurrentPhrase(currentPhrase+1);
+      setCurrentPhrase(currentPhrase + 1);
       setCurrentChar(0);
     }, newLineDelay);
-  }
+  };
 
   useEffect(() => {
     setTimeout(() => {
-        switch (isDeleting) {
-          case true:
-            if (currentChar > 0) {
-              setCurrentChar(currentChar - 1);
+      switch (isDeleting) {
+        case true:
+          if (currentChar > 0) {
+            setCurrentChar(currentChar - 1);
+          } else {
+            setIsDeleting(false);
+            if (currentPhrase < phrases.length - 1) {
+              setCurrentPhrase(currentPhrase + 1);
             } else {
-              setIsDeleting(false);
-              if (currentPhrase < phrases.length - 1) {
-                setCurrentPhrase(currentPhrase + 1);
-              } else {
-                setCurrentPhrase(0);
-              }
+              setCurrentPhrase(0);
             }
-            break;
-          case false:
-            if (currentChar < phrases[currentPhrase].length) {
-              setCurrentChar(currentChar + 1);
-            } else {
-              variation === TextCycle.LOOP ? beginDeleting() : addNewLine();
-            }
-            break;
-        }
+          }
+          break;
+        case false:
+          if (currentChar < phrases[currentPhrase].length) {
+            setCurrentChar(currentChar + 1);
+          } else {
+            variation === TextCycle.LOOP ? beginDeleting() : addNewLine();
+          }
+          break;
+      }
     }, typeSpeed);
   });
 
   return (
     <div className={styles.container}>
-    <span>
-    { newLine ? <br/> : ''}{phrases[currentPhrase].substring(0, currentChar)}
-      <span className={styles.cursor}>{"▎"}</span>
-    </span>
+      <span>
+        {newLine ? <br /> : ""}
+        {phrases[currentPhrase].substring(0, currentChar)}
+        <span className={styles.cursor}>{"▎"}</span>
+      </span>
     </div>
   );
 };
